59a716a5795786b57e9aa9953b1483d4dd7e1acc
Anastasiia Kirianova <111008060+tinykiri@users.noreply.github.com>
2024-01-12 21:00:15 +0200

    blablabla

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-console.log('hello git');
-
-console.log(1);
-console.log(2);
-console.log(3);
-
-console.log("peach.js");
-console.log("popka");
-
-console.log('new change');
+console.log('vse huynya');
